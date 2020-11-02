b80edd9036cc2691790f4f947854622651e20b2d
Kym Hooper <khoupp@gmail.com>
2020-11-02 16:34:03 -0700

    Refactor the structure for one card component

--- my-app/src/TarotCard.jsx
+++ my-app/src/TarotCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-// import from;
+import Image from 'react-bootstrap/Image'
 
 const TarotCard = ({card}) => {
-  return (
+
+  return card === false ? (<div></div>) : (
     <div>
       <span>{card.name}</span>
+      <br></br>
+      <img src={card.url} width="200" height="350"></img>
       <p>{card.desc}</p>
       <p>{card.meaning_rev}</p>
       <p>{card.meaning_up}</p>
