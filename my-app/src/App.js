3a00070b9d57f9a41f546a872658d1aca7fead60
Kym Hooper <khoupp@gmail.com>
2020-11-02 16:35:24 -0700

    Add state to toggle the entire list, one card, and three cards to display

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,32 @@
 import './App.css'
 import React from 'react';
 import axios from 'axios';
+import shuffle from 'lodash/shuffle';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image'
+import Col from 'react-bootstrap/Col'
 import TarotCardList from './TarotCardList.jsx';
+import photos from './photos.js';
 import Search from './Search.jsx';
 import DisplayOne from './DisplayOne.jsx';
-
+import DisplayThree from './DisplayThree.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       cardList: [],
-      imageList: [],
-      searchName: '',
-      // searchKW: '',
-      cardToDisplay: null,
+      showDeck: false,
+      search: '',
+      cardToDisplay: false,
+      cardListToDisplay: [],
     };
-    this.handleSearchChangeName = this.handleSearchChangeName.bind(this);
+    this.getAllCards = this.getAllCards.bind(this);
+    this.toggleDeckVisability = this.toggleDeckVisability.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
     this.clickOne = this.clickOne.bind(this);
+    this.clickThree = this.clickThree.bind(this);
   }
   componentDidMount() {
     this.getAllCards();
@@ -29,69 +35,79 @@ class App extends React.Component {
   getAllCards() {
     axios.get(`https://rws-cards-api.herokuapp.com/api/v1/cards`)
         .then((res) => {
-          console.log(res.data.cards);
-          this.setState({cardList: res.data.cards});
+          const mappedList = res.data.cards.map((card, i) => {
+            card.url = photos[i];
+            return card});
+          this.setState({cardList: mappedList});
         })
         .catch((err) => console.error(err));
   }
 
-  getAllImages() {
-    axios.get(`https://tarot.howlcode.com/api/v1`)
-    .then((res) => {
-      console.log('HI!', res.data);
-      this.setState({imageList: res.data});
-    })
-    .catch((err) => console.error(err));
-  }
-
-  handleSearchChangeName(e) {
-    this.setState({searchName: e.target.value.toLowerCase()});
+  handleSearch(e) {
+    this.setState({search: e.target.value.toLowerCase()});
   }
 
   clickOne() {
-    let card = this.state.cardList[Math.floor(Math.random() * this.state.cardList.length)];
-    console.log('card', card);
+    let card = this.state.cardList[Math.floor(Math.random() * 78)];
     this.setState({
       cardToDisplay: card
     });
   }
 
+  toggleDeckVisability(e) {
+    e.preventDefault();
+    this.setState({showDeck : !this.state.showDeck});
+  }
 
+  clickThree() {
+    let threeCards = shuffle(this.state.cardList).slice(-3);
+    this.setState({
+      cardListToDisplay: threeCards
+    });
+  }
 
   render() {
-    const {cardList, searchName, cardToDisplay} = this.state;
+    const {cardList, search, cardToDisplay, cardListToDisplay, showDeck} = this.state;
     let filteredTarot;
-    searchName.length >= 3
+    search.length >= 3
       ? filteredTarot = cardList.filter((card) =>
-      card.name.toLowerCase().includes(searchName.toLowerCase()) ||
-      card.meaning_rev.toLowerCase().includes(searchName.toLowerCase()) ||
-      card.meaning_up.toLowerCase().includes(searchName.toLowerCase()))
+      card.name.toLowerCase().includes(search.toLowerCase()) ||
+      card.meaning_rev.toLowerCase().includes(search.toLowerCase()) ||
+      card.meaning_up.toLowerCase().includes(search.toLowerCase()))
       : filteredTarot = cardList;
     return (
       <div>
-        <h1>gain insight into the past, present, or future</h1>
-        <Image src="https://i5.walmartimages.com/asr/366e8cb3-08a9-4ba0-8f7a-d04d0a5a2398_1.fe42737149d0bdaafa44fb0ec7192d8c.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff" fluid />
-        <Image src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/11-hands-on-crystal-ball-and-cryptocurrency-allan-swart.jpg" fluid />
-
         <Container className="tarot-container">
+          <h1>gain insight into the past, present, or future</h1>
+          {/* <Col> */}
+          <Image src="https://i5.walmartimages.com/asr/366e8cb3-08a9-4ba0-8f7a-d04d0a5a2398_1.fe42737149d0bdaafa44fb0ec7192d8c.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff" className="crystal-ball-img" width="400" height="400" />
+          {/* </Col> */}
           <Search
-            searchName={searchName}
-            handleSearchChangeName={this.handleSearchChangeName}
-          />
+            search={search}
+            handleSearch={this.handleSearch} />
+          <Button
+            variant="secondary"
+            onClick={this.toggleDeckVisability}
+          >Explore the deck</Button>
           <Button
             variant="secondary"
             onClick={this.clickOne}
-          >Let fate generate a card for you!</Button>
+          >Let fate choose a card for you!</Button>
+          <Button
+            variant="secondary"
+            onClick={this.clickThree}
+          >Three-card reading</Button>
           <div>
             <DisplayOne
               card={cardToDisplay}
             />
+            <DisplayThree
+              cardListToDisplay={cardListToDisplay}
+            />
           </div>
-          <TarotCardList
-          cardList={filteredTarot}
-          // cardToDisplay={cardToDisplay}
-          clickOne={this.clickOne}
-          />
+          {this.state.showDeck ? <TarotCardList
+            cardList={filteredTarot}
+          /> : <div></div>}
         </Container>
       </div>
     );
