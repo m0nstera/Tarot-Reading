fd0494fab8b7f9db8c2292a2b85f13c83c4b3fb9
Kym Hooper <khoupp@gmail.com>
2020-11-06 12:28:37 -0700

    Code clean up

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -9,10 +9,6 @@ app.use(express.static('../public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// app.get();
-
-// app.post();
-
 app.listen(PORT, () => {
   console.log(`Server running and listening on port: ${PORT}`);
 });
